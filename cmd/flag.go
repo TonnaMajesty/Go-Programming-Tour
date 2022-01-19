c75482e46b074c3ad77bf1def10bf58b4c75af68
TonnaMajesty <tonnatzh54@163.com>
2022-01-19 14:45:11 +0800

    add word cmd

---
+++ cmd/flag.go
@@ -0,0 +1,15 @@
+package cmd
+
+import (
+	"flag"
+	"log"
+)
+
+func flag_t() {
+	var name string
+	flag.StringVar(&name, "name", "Go 语言编程之旅", "帮助信息")
+	flag.StringVar(&name, "n", "Go 语言编程之旅", "帮助信息")
+	flag.Parse()
+
+	log.Printf("name: %s", name)
+}
---
+++ cmd/sub_command.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"flag"
+	"log"
+)
+
+var name string
+
+func flag_sub() {
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+
+	switch args[0] {
+	// go run main.go go -name=eddycjy
+	case "go":
+		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+		goCmd.StringVar(&name, "name", "Go 语言", "帮助信息")
+		_ = goCmd.Parse(args[1:])
+	// go run main.go php -n=tonna
+	case "php":
+		phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+		phpCmd.StringVar(&name, "n", "PHP 语言", "帮助信息")
+		_ = phpCmd.Parse(args[1:])
+	}
+
+	log.Printf("name: %s", name)
+}
